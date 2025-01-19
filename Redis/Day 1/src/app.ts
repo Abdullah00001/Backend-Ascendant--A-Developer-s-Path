2e7689f0f7a2f91ce816a07cef79499e4ab95ea4
Abdullah Bin Omar Chowdhury <abdullahbinomarchowdhury@gmail.com>
2025-01-19 11:32:16 +0600

    familier with redis

--- Redis/Day 1/src/app.ts
+++ Redis/Day 1/src/app.ts
@@ -1,20 +1,23 @@
 import express, { Application, Request, Response } from "express";
 import redisClient from "./configs/redis.configs.js";
-import { Document } from "mongoose";
 import { Todo } from "./models/todo.model.js";
+import { Document } from "mongoose";
 
 const app: Application = express();
 
 app.use(express.json());
+app.set("trust proxy", true);
 
 app.get("/", async (req: Request, res: Response): Promise<void> => {
   const cachedTodos = await redisClient.get("todos");
+  const ip = req.ip;
+  console.log(ip);
   if (cachedTodos) {
     res.status(200).json({ data: JSON.parse(cachedTodos) });
     return;
   } else {
-    const todos = await Todo.find({});
-    await redisClient.set("todos", JSON.stringify(todos));
+    const todos: Document[] = await Todo.find({});
+    await redisClient.set("todos", JSON.stringify(todos), { EX: 60 });
     res.status(200).json({ data: todos });
     return;
   }
