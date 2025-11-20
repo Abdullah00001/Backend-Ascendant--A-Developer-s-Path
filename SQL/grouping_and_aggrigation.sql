3be60e7a20341bff8877af4e4f6be4374060b838
abdullah00001 <abdullahbinomarchowdhury@gmail.com>
2025-11-20 23:58:16 +0600

    recaping grouping and aggrigation

--- SQL/grouping_and_aggrigation.sql
+++ SQL/grouping_and_aggrigation.sql
@@ -3,3 +3,15 @@ FROM team_members
 WHERE team_id=48869
 ORDER BY id DESC
 LIMIT 100;
+
+SELECT team_id, COUNT(id)
+FROM projects
+GROUP BY team_id
+ORDER BY team_id DESC
+LIMIT 10;
+
+SELECT created_by,COUNT(id)
+FROM tasks
+GROUP BY created_by
+HAVING COUNT(id)>5
+LIMIT 10;
