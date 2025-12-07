eec10397adf221b25c0e7c293c94abf0d06cc226
abdullah00001 <abdullahbinomarchowdhury@gmail.com>
2025-12-07 13:48:41 +0600

    subquery mistake retake complate

--- SQL/subquery_mistake_retake.sql
+++ SQL/subquery_mistake_retake.sql
@@ -1,14 +1,21 @@
 --Subquery Retake Exam On 7 Mistake's
 
--- Retrieve the tasks that are the newest task inside their own project.
+-- Retrieve the tasks that are the newest task inside their own project. (Solved)
+
+WITH newest_tasks_date_per_project AS (
+	SELECT project_id,MAX(created_at) AS created_at
+	FROM tasks
+	GROUP BY project_id
+)
 
 SELECT project_id,title,id
-FROM tasks
-WHERE created_at = (
-	SELECT MAX(created_at)
-	FROM tasks
+FROM tasks t
+WHERE EXISTS (
+	SELECT 1
+	FROM newest_tasks_date_per_project AS ntdpp
+	WHERE ntdpp.project_id=t.project_id AND ntdpp.created_at=t.created_at
 )
-GROUP BY project_id,title,id
+ORDER BY project_id ASC
 LIMIT 10;
 
 -- Find all users who do NOT have any activity logs.
@@ -55,30 +62,39 @@ HAVING COUNT(id)> (
 )
 LIMIT 10;
 
--- Select tasks that have the highest priority in their own project.
+-- Select tasks that have the highest priority in their own project. (Solved)
 
-SELECT t.project_id AS project_id, t.id AS task_id, t.priority AS priority
-FROM tasks AS t
-GROUP BY t.project_id,t.id,t.priority
-HAVING t.priority = (
-	SELECT MAX(priority)
+SELECT *
+FROM tasks AS t1
+WHERE (project_id,priority) IN (
+	SELECT project_id,priority
 	FROM tasks
+	GROUP BY project_id,priority
+	HAVING priority='HIGH'
 )
-ORDER BY project_id ASC
-LIMIT 50;
 
--- Show all users who HAVE at least one task created by them.
+-- Show all users who HAVE at least one task created by them. (Solved)
 
-SELECT t.created_by
-FROM tasks AS t
-GROUP BY t.created_by
-HAVING COUNT(t.id)>1
+SELECT *
+FROM users AS u
+WHERE EXISTS (
+	SELECT 1
+	FROM tasks AS t
+	WHERE u.id=t.created_by
+	GROUP BY t.created_by
+	HAVING COUNT(t.id)>0
+)
 LIMIT 10;
 
--- In the tasks table, list all days (date only) where the number of tasks created on that day is greater than 5.
 
-SELECT t.created_at
-FROM tasks t
-GROUP BY t.created_at
-HAVING COUNT(t.id)>5
-;
+-- In the tasks table, list all days (date only) where the number of tasks created on that day is greater than 5. (Solved)
+
+SELECT DISTINCT Date(created_at)
+FROM tasks AS t1
+WHERE Date(created_at) IN (
+	SELECT DISTINCT Date(created_at)
+	FROM tasks 
+	GROUP BY Date(created_at)
+	HAVING COUNT(id)>5
+);
+
