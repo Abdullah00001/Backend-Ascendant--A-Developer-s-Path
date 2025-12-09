c1d7c2611cf381266a13acb6d3e040679259709e
abdullah00001 <abdullahbinomarchowdhury@gmail.com>
2025-12-09 23:23:34 +0600

    learned 1nf

--- SQL/normalization.sql
+++ SQL/normalization.sql
@@ -4,3 +4,6 @@
     -- Requirements
 
     -- [1] Each Column Contain Atomic (Indivisible) Values
+    -- [2] Each Column Contain's Value's of The Same Type
+    -- [3] Each Now Is Unique (Ensured By A Primary Key)
+    -- [4] No Repeating Group Of Column's
